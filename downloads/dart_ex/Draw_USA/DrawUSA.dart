de7b88222af4a6b7154a924f0fc93d5c490af87c
s40523241 <40523241@gm.nfu.edu.tw>
2019-12-26 14:27:27 +0800

    W16 查驗考試 更改

--- downloads/dart_ex/Draw_USA/DrawUSA.dart
+++ downloads/dart_ex/Draw_USA/DrawUSA.dart
@@ -1,3 +1,14 @@
+import 'dart:html';
+import 'dart:math' as Math;
+
+CanvasElement canvas;
+CanvasRenderingContext2D ctx;
+int flag_w = 300;
+int flag_h = 200;
+num circle_x = flag_w / 4;
+num circle_y = flag_h / 4;
+
+
 void drawUSA(ctx){
   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(255, 255, 255)';
@@ -18,3 +29,4 @@ void drawUSA(ctx){
   ctx.fillRect(0, 180, flag_w, flag_h / 13);
   ctx.fillStyle = 'rgb(0, 0, 150)';
   ctx.fillRect(0, 0, flag_w / 2, flag_h / 2);
+  }
