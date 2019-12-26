4afc1b5b322ce624e8c0102728a2f971d37c1b13
s40523241 <40523241@gm.nfu.edu.tw>
2019-12-26 14:10:35 +0800

    W16 查驗考試

---
+++ downloads/dart_ex/Draw_USA/DrawUSA.dart
@@ -0,0 +1,20 @@
+void drawUSA(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 255, 255)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 30, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 60, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 90, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 120, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 150, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 180, flag_w, flag_h / 13);
+  ctx.fillStyle = 'rgb(0, 0, 150)';
+  ctx.fillRect(0, 0, flag_w / 2, flag_h / 2);
